42a94cccc45e2486ec80e735349a4eb713032af5
Dominic Lind <dominiclind@Dominics-iMac.local>
2014-04-21 13:16:39 +0200

    added rAF
    hope of performance increase.
    
    stopped uncessesary hidden animations.

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,32 @@ var select = function(selector){
 	return document.querySelector(selector);
 };
 
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+
 var spotifyScrollView = {
 
 	iscroll : null,
@@ -21,56 +47,74 @@ var spotifyScrollView = {
 	},
 
 	onScroll : function(end){
-
+		var _this = this;
 		// animate that bg on down scroll
-		if(this.y < spotifyScrollView.bg_height){
-			$(spotifyScrollView.bg_image).css({
-				'background-position-y' : -(this.y / 2) + 'px'
-			});
-		}
+		requestAnimationFrame(function(){
+			if(_this.y < spotifyScrollView.bg_height){
 
-		// make portrait fixed
-		if(this.y < 0){
-			$(spotifyScrollView.artist_info).css({
-				'-webkit-transform' : 'translateY('+ Math.abs(this.y) +'px)'
-			});
-		}else{
-			$(spotifyScrollView.artist_info).css({
-				'-webkit-transform' : 'translateY(0px)'
-			});
-		}
+				$(spotifyScrollView.bg_image).css({
+					'background-position-y' : -(_this.y / 2) + 'px'
+				});
+			}
 
-		// animate away portrait & make bg big and not blurry
-		if(this.y > 0){
-			var percent = Math.abs(this.y) / spotifyScrollView.bg_threshold;
+			// make portrait fixed
+			if(this.y < 0){
+				$(spotifyScrollView.artist_info).css({
+					'-webkit-transform' : 'translateY('+ Math.abs(_this.y) +'px)'
+				});
+			}else{
+				$(spotifyScrollView.artist_info).css({
+					'-webkit-transform' : 'translateY(0px)'
+				});
+			}
 
-			// fade artist
-			$(spotifyScrollView.artist_info).css({
-				'opacity' : 1 - (0.1 + percent)
-			});
+			// animate away portrait & make bg big and not blurry
+			if(_this.y > 0){
+				var percent = Math.abs(_this.y) / spotifyScrollView.bg_threshold;
 
-			// bg pic
-			$(spotifyScrollView.bg_image).css({
-				//'-webkit-filter' : 'blur(' + (12 - (12 * percent) ) + 'px)',
-				'background-position-y' : '0px',
-				'-webkit-transform' : 'translateY('+ -( Math.abs(this.y) / 2) +'px) scale('+ (1 + (0.5 * percent) )+')',
-				'opacity' : (0.4 + percent)
-			});
-		}
+				// fade artist
+
+				$(spotifyScrollView.artist_info).css({
+					'opacity' : 1 - (0.1 + percent)
+				});
+
+				// bg pic
+				$(spotifyScrollView.bg_image).css({
+					//'-webkit-filter' : 'blur(' + (12 - (12 * percent) ) + 'px)',
+					'background-position-y' : '0px',
+					'-webkit-transform' : 'translateY('+ -( Math.abs(_this.y) / 2) +'px) scale('+ (1 + (0.5 * percent) )+')',
+					'opacity' : (0.4 + percent)
+				});
+			}
 
 
-		// stick tat shuffle btn
-		if(Math.abs(this.y) > spotifyScrollView.bg_height - 55){
-			$('.shuffle').addClass('sticky');
-			$('.shuffle').css({
-				'-webkit-transform' : 'translateY('+ ( Math.abs(this.y) - ( spotifyScrollView.bg_height - 55) ) +'px)'
-			})
-		}else{
-			$('.shuffle').removeClass('sticky');
-			$('.shuffle').css({
-				'-webkit-transform' : 'translateY(0px)'
-			})
-		}
+			// stick tat shuffle btn
+			if(Math.abs(_this.y) > spotifyScrollView.bg_height - 55){
+				$('.shuffle').addClass('sticky');
+				$('.shuffle').css({
+					'-webkit-transform' : 'translateY('+ ( Math.abs(_this.y) - ( spotifyScrollView.bg_height - 55) ) +'px)'
+				})
+
+				// remove unnessesary stuffs
+				$(spotifyScrollView.bg_image).css({
+					//'-webkit-filter' : 'blur(' + (12 - (12 * percent) ) + 'px)',
+					'background-position-y' : '0px',
+					'-webkit-transform' : 'translateY(0px)',
+					'opacity' : 1
+				});
+				$(spotifyScrollView.artist_info).css({
+					'opacity' : 1,
+					'-webkit-transform' : 'translateY(0px)'
+				});
+
+
+			}else{
+				$('.shuffle').removeClass('sticky');
+				$('.shuffle').css({
+					'-webkit-transform' : 'translateY(0px)'
+				})
+			}
+		});
 	},
 
 	getSongListItem : function(songName) {
